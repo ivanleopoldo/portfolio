2371c2d7d71e54f958a3152dd876c7b5430be751
ivaintwc <75525118+ivanleopoldo@users.noreply.github.com>
2024-09-05 01:56:58 +0800

    Update route.ts

--- src/app/api/download/[filename]/route.ts
+++ src/app/api/download/[filename]/route.ts
@@ -8,7 +8,7 @@ export async function GET(req: Request, { params }: GetParams) {
   const filename = params.filename;
 
   const RESUME =
-    "https://drive.google.com/uc?export=download&id=1lPs6_zd6fGLHq0NNFpfrufZT6R2Yj4Kf/";
+    "https://drive.google.com/uc?export=download&id=1lPs6_zd6fGLHq0NNFpfrufZT6R2Yj4Kf";
 
   const response = await fetch(RESUME);
 
